7676f5294ee3e9c7e40806c230c88ec63a205072
RAILALA Andriatsimarivo (Andrew) <r3andrew@gmail.com>
2016-11-10 15:36:57 +0800

    database setup

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://cp3junior:cp3killerCROSS@ds050559.mlab.com:50559/expose');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('database connected');
+});
+
+var schemaUser = mongoose.Schema({
+	name: String,
+	email: String
+});
+var userModel = mongoose.model('userModel', schemaUser);
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,8 +26,25 @@ app.use(express.static(__dirname + '/public'));
 // app.set('views', __dirname + '/views');
 // app.set('view engine', 'ejs');
 
+var me = {
+	name: "andrew",
+	email: "r3andrew@gmail.com"
+};
+
 app.get('/', function(req, res) {
-  res.send('Hello world');
+  //var data = new userModel(me);
+  //data.save();
+
+  // var out = {};
+
+  userModel.find(function(err, resp){
+  	if (err){
+  		res.send(err);
+  	}
+  	res.json(resp);
+  });
+
+  
 });
 
 app.listen(app.get('port'), function() {
