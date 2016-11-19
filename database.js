8d44b62763da8a874bc869c775f24e7fa527b60d
RAILALA Andriatsimarivo (Andrew) <r3andrew@gmail.com>
2016-11-19 16:51:09 +0800

    modif db

--- database.js
+++ database.js
@@ -3,7 +3,10 @@ var cred = require('./credentials');
 var dbUser = cred.c.dbUser;
 var dbPass = cred.c.dbPass;
 
+// mongoose.connect('mongodb://'+dbUser+':'+dbPass+'@ds157667.mlab.com:57667/exposedb');
 mongoose.connect('mongodb://'+dbUser+':'+dbPass+'@ds050559.mlab.com:50559/expose');
+
+//mongodb://<dbuser>:<dbpassword>@ds157667.mlab.com:57667/exposedb
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
