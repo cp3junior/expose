d1eeaafa9bd80ed28c295c1f68fbf9122b97a992
RAILALA Andriatsimarivo (Andrew) <r3andrew@gmail.com>
2016-11-21 17:21:50 +0800

    update cors for post using flight n other

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ var router = require('./routes');
 
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
